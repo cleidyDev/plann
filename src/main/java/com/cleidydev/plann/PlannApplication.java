876cbaf16acdb68c990890362bf45ffc509d7dbc
Cleidy Fumanca <cleidydev@gmail.com>
2026-01-20 11:59:51 +0100

    First commit

---
+++ src/main/java/com/cleidydev/plann/PlannApplication.java
@@ -0,0 +1,13 @@
+package com.cleidydev.plann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleidydev/plann/PlannApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleidydev.plann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
